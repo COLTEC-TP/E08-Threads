4946857bba1998fc2a775b26a8b86c3ea05c8451
joao gabriel fernandes zenobio <a2016953386@la0012217603.infocoltec.local>
2018-10-01 09:06:23 -0300

    E08

---
+++ E08-Threads/src/CorridaDeSapos.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CorridaDeSapos {
+
+    public static void main(String[] args) {
+
+        ArrayList<Thread> threads = new ArrayList();
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = reader.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(i + " - jump: ");
+            int distanciaPulo = reader.nextInt();
+            Thread threadSapo = new Thread(new Sapo(distanciaPulo, i));
+            threads.add(threadSapo);
+        }
+
+        for(int i = 0; i < n; i++) {
+            threads.get(i).start();
+        }
+
+        reader.close();
+    }
+}
